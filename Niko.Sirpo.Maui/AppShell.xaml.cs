a18dd405b95f841f60c97335c4ffe164824d5b1c
niko <saariniemi@gmail.com>
2023-06-02 18:08:11 +0200

    Created project

---
+++ Niko.Sirpo.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Niko.Sirpo.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Niko.Sirpo.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Niko.Sirpo.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
